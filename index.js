9a3a07b29a412dbe650444d70d991d20589210a3
Guillaume Badi <gbadi@mailjet.com>
2015-11-16 18:42:14 +0100

    init

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-module.exports = require ('./lib/lanbda');
+function req (name) {
+  return require ('./lib/' + name);
+}
+
+module.exports = {
+  req('complement'),
+  req('defn'),
+  req('inc'),
+  req('partial'),
+}
+
+console.log(module.exports);
