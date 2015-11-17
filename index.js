a4ddc3fe54ba2894e9adf43c221723d1e16165d1
Guillaume Badi <gbadi@mailjet.com>
2015-11-17 10:40:10 +0100

    addings

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
 
 /*
+  Higher order map
+
+  Usage:
+    map([1,2,3,4], i => i + 1);
+*/
+export function map (array, action) {
+  return array.map(action);
+}
+
+/*
+  Higher order filter
+
+  Usage:
+    filter([1, 'hey', '3', 4], !isNaN);
+
+    // => [1, 3, 4]
+*/
+export function filter (array, fn) {
+  return array.filter(fn)
+}
+
+/*
+  Higher order reduce
+
+  Usage:
+    reduce([1,2,3,4], (i, j) => i + j);
+*/
+export function reduce (array, fn, init = 0) {
+  return array.reduce(fn, init);
+}
+
+/*
+  Higher order forEach
+
+  Usage:
+    forEach([1,2,3,4], i => console.log (i))
+*/
+export function forEach (array, action) {
+  return array.forEach(action);
+}
+
+/*
+  Higher order object getter
+
+  Usage:
+    [{name: guillaume, age: 21}, {name: donald, age: 23}]
+      .map(partial(get, null, 'name'))
+
+    // => [guillaume, donald]
+*/
+export function get (obj, key) {
+  return obj[key];
+}
+
+/*
   Define an overloaded function
   returns a function that select the function to use depending on
   the number of parameters passed.
@@ -63,18 +118,6 @@ export function* range (min = 0, max = Infinity, step = 1) {
 }
 
 /*
-  Takes an array `array` and a new array 'nArray'
-  and returns a final array filled with the new values
-
-  Usage:
-    fillArray([3, 5], [1, 2, null, 3, null]) // => [1, 2, 3, 3, 5]
-*/
-function fillArray (nArray, array) {
-  let i = 0;
-  return array.map((e) => !e ? nArray[i++] : e);
-}
-
-/*
   Take a function fn and an array of args with empty spaces
   and returns a partial function `g` that take a single arg
 
@@ -89,5 +132,16 @@ function fillArray (nArray, array) {
 
 */
 export default function partial (fn, ...args) {
+  /*
+    Takes an array `array` and a new array 'nArray'
+    and returns a final array filled with the new values
+
+    Usage:
+      fillArray([3, 5], [1, 2, null, 3, null]) // => [1, 2, 3, 3, 5]
+  */
+  function fillArray (nArray, array) {
+    let i = 0;
+    return array.map((e) => !e ? nArray[i++] : e);
+  }
   return (...nArgs) => fn.apply(null, fillArray(nArgs, args));
 }
